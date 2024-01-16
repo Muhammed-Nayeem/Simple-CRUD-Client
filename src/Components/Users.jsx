a3bf46dfc7a69fd3d05b9e1d8ef5536e3f2a2ad0
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-01-16 08:44:28 +0600

    User creation and deletion

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-  const users = useLoaderData();
-  console.log(users);
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+
+  const deleteUserHandler = (_id) => {
+    console.log(`User Id is: ${_id}`);
+
+    //delete user by api:
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("User deleted successfully!");
+          const remaining = users.filter((user) => user._id !== _id);
+          setUsers(remaining);
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
   return (
     <section>
@@ -10,11 +32,25 @@ const Users = () => {
         <h2 style={{ margin: "20px 0" }}>Total Users: {users.length}</h2>
         <ul>
           {users.map((user) => (
-            <li key={user?._id}>
+            <li
+              style={{
+                margin: "10px 0",
+                display: "flex",
+                alignItems: "center",
+                gap: "8px",
+              }}
+              key={user?._id}
+            >
               <p>
                 Name: {user?.name}, Profession: {user?.profession}, Email:{" "}
                 {user?.email}
               </p>
+              <button
+                onClick={() => deleteUserHandler(user?._id)}
+                style={{ padding: "4px 15px", cursor: "pointer" }}
+              >
+                X
+              </button>
             </li>
           ))}
         </ul>
